fb5b80f4f61ad2c5fe6b7a41807bbae9ce237623
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-05-21 13:41:15 +0100

    plugins, views, webpack, app insights, logging etc

---
+++ app/routes/static.js
@@ -0,0 +1,49 @@
+const config = require('../config')
+
+module.exports = [{
+  method: 'GET',
+  path: '/robots.txt',
+  options: {
+    handler: {
+      file: 'app/static/robots.txt'
+    },
+    cache: {
+      expiresIn: config.staticCacheTimeoutMillis,
+      privacy: 'private'
+    }
+  }
+}, {
+  method: 'GET',
+  path: '/assets/{path*}',
+  options: {
+    handler: {
+      directory: {
+        path: [
+          'node_modules/govuk-frontend/govuk/assets'
+        ]
+      }
+    },
+    cache: {
+      expiresIn: config.staticCacheTimeoutMillis,
+      privacy: 'private'
+    }
+  }
+}, {
+  method: 'GET',
+  path: '/static/{path*}',
+  options: {
+    handler: {
+      directory: {
+        path: [
+          'app/dist',
+          'node_modules/govuk-frontend/govuk/assets',
+          'app/static'
+        ]
+      }
+    },
+    cache: {
+      expiresIn: config.staticCacheTimeoutMillis,
+      privacy: 'private'
+    }
+  }
+}]
