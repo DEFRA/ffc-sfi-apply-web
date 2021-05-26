292af2b5e2f5bf78482ce1dc2c3c00f722d63fea
Fay Toward <fay.toward@rpa.gov.uk>
2021-05-26 13:21:09 +0100

    bank details section

--- app/routes/payment-details/payment-frequency.js
+++ app/routes/payment-details/payment-frequency.js
@@ -14,16 +14,8 @@ module.exports = [{
   method: 'POST',
   path: '/payment-details/payment-frequency',
   options: {
-    validate: {
-      payload: joi.object({
-        paymentFrequency: joi.string().required()
-      }),
-      failAction: async (request, h, error) => {
-        return h.view('payment-details/payment-frequency', new ViewModel(request.payload.paymentFrequency, error)).code(400).takeover()
-      }
-    },
     handler: async (request, h) => {
-      return h.redirect('bank-details')
+      return h.redirect('/application-task-list')
     }
   }
 }]
