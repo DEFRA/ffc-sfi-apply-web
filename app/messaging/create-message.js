d2ae90de94d833a982f026ef48b9d3e4ddaa38f8
John Watson <johnwatson484@hotmail.com>
2021-05-28 12:01:08 +0100

    Dummy correlation added

--- app/messaging/create-message.js
+++ app/messaging/create-message.js
@@ -2,7 +2,8 @@ function createMessage (body, type) {
   return {
     body,
     type,
-    source: 'ffc-sfi-apply-web'
+    source: 'ffc-sfi-apply-web',
+    correlationId: 'f34dfc96-865b-4116-917a-3d6db2231479'
   }
 }
 
