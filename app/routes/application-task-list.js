925c371884d683a80010dcfff035c082d6ceeb0d
Fay Toward <fay.toward@rpa.gov.uk>
2021-06-02 09:45:29 +0100

    Co-authored-by: steve-dickinson <steve-dickinson@users.noreply.github.com>

--- app/routes/application-task-list.js
+++ app/routes/application-task-list.js
@@ -1,8 +1,12 @@
+const sessionHandler = require('../session/session-handler')
+
 module.exports = {
   method: 'GET',
   path: '/application-task-list',
   options: {
     handler: (request, h) => {
+      const agreement = sessionHandler.get(request, 'agreement')
+      console.log(agreement)
       return h.view('application-task-list')
     }
   }
