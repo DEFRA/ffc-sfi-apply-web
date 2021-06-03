d3546cd0d1b45507d1f4073ea4be7c4b2f76a735
John Watson <johnwatson484@hotmail.com>
2021-06-03 20:11:49 +0100

    Status model added

---
+++ app/data/models/status.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const status = sequelize.define('status', {
+    statusId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    status: DataTypes.STRING
+  },
+  {
+    tableName: 'agreements',
+    freezeTableName: true
+  })
+  status.associate = function (models) {
+    status.hasMany(models.agreement, {
+      foreignKey: 'statusId',
+      as: 'agreements'
+    })
+  }
+  return status
+}
