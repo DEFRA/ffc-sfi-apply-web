0d2cfc6a649c3af76f4a9c654e1cfe2166d05c8f
Fay Toward <fay.toward@rpa.gov.uk>
2021-06-08 10:42:36 +0100

    adding map

--- app/frontend/js/map.js
+++ app/frontend/js/map.js
@@ -2,70 +2,141 @@ import 'ol/ol.css'
 import GeoJSON from 'ol/format/GeoJSON'
 import Map from 'ol/Map'
 import View from 'ol/View'
-import { Fill, Stroke, Style } from 'ol/style'
-import { BingMaps, Vector as VectorSource } from 'ol/source'
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
-import { get as getProjection } from 'ol/proj'
-import proj4 from 'proj4'
-import { register } from 'ol/proj/proj4'
-
-const styles = {
-  Polygon: new Style({
-    stroke: new Stroke({
-      color: 'red',
-      width: 1
-    }),
-    fill: new Fill({
-      color: 'rgba(249, 6, 44, 0.1)'
-    })
-  })
-}
+import { XYZ, Vector as VectorSource } from 'ol/source'
+import { Tile as TileLayer, Vector as VectorLayer, Group } from 'ol/layer'
+import Select from 'ol/interaction/Select'
+import { click, pointerMove } from 'ol/events/condition'
+import TileGrid from 'ol/tilegrid/TileGrid'
+import { landParcelStyles, landCoverStyles, highlightStyle, pointerMoveStyle } from './map-styles'
 
 const styleFunction = (feature) => {
-  return styles[feature.getGeometry().getType()]
+  const label = `${feature.get('sheet_id')} ${feature.get('parcel_id')}`
+
+  if (feature.get('land_cover_class_code') !== undefined) {
+    const landCoverClassCode = feature.get('land_cover_class_code')
+
+    const landCoverClassCodeStyle = landCoverStyles.find(({ Code }) => Code === landCoverClassCode)
+
+    if (landCoverClassCodeStyle !== null && landCoverClassCodeStyle !== undefined) {
+      landCoverClassCodeStyle.Polygon.getText().setText(label)
+      return landCoverClassCodeStyle[feature.getGeometry().getType()]
+    } else {
+      landCoverStyles[0].Polygon.getText().setText(label)
+      return landCoverStyles[0][feature.getGeometry().getType()]
+    }
+  } else {
+    landParcelStyles.Polygon.getText().setText(label)
+    return landParcelStyles[feature.getGeometry().getType()]
+  }
 }
 
-const createBingMapsSource = () => {
-  return new BingMaps({
-    key: 'AvlstdycF2zG8HdPPAPv29mJrVMFi3ixiv9Tt4LiqR3Bt9QQNE9wqK02H3IeOzAp',
-    imagerySet: 'AerialWithLabelsOnDemand',
-    culture: 'en-GB',
-    maxZoom: 19
-  })
+const tilegrid = new TileGrid({
+  resolutions: [896.0, 448.0, 224.0, 112.0, 56.0, 28.0, 14.0, 7.0, 3.5, 1.75],
+  origin: [-238375.0, 1376256.0]
+})
+
+const hightlightOnMouseOver = (parcelSource) => {
+  document.querySelectorAll('#parcels tr').forEach(e => e.addEventListener('mouseover', () => {
+    if (e.id) {
+      const selectedFeature = parcelSource.getFeatureById(e.id)
+      selectedFeature.setStyle(highlightStyle)
+    }
+  }))
+
+  document.querySelectorAll('#parcels tr').forEach(e => e.addEventListener('mouseout', () => {
+    if (e.id) {
+      const selectedFeature = parcelSource.getFeatureById(e.id)
+      selectedFeature.setStyle(landParcelStyles)
+    }
+  }))
 }
 
-const createprojection = () => {
-  proj4.defs(
-    'EPSG:27700',
-    '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 ' +
-      '+x_0=400000 +y_0=-100000 +ellps=airy ' +
-      '+towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 ' +
-      '+units=m +no_defs'
-  )
+const mapStyles = [
+  'Road_27700',
+  'Outdoor_27700',
+  'Light_27700']
 
-  register(proj4)
-  return getProjection('EPSG:27700')
+const buildMapLayers = (parcelSource, apiKey) => {
+  const parcelLayer = new VectorLayer({ source: parcelSource, style: styleFunction })
+
+  const layers = []
+
+  const mapStyleLayers = mapStyles.length
+
+  for (let i = 0; i < mapStyleLayers; ++i) {
+    layers.push(
+      new TileLayer({
+        title: 'Road',
+        type: 'base',
+        visible: false,
+        source: new XYZ({
+          url: `https://api.os.uk/maps/raster/v1/zxy/${mapStyles[i]}/{z}/{x}/{y}.png?key=${apiKey}`,
+          tileGrid: tilegrid
+        })
+      })
+    )
+  }
+
+  layers.push(parcelLayer)
+
+  return layers
 }
 
-export function displayMap (parcels, coordinates) {
+export function displayMap (apiKey, sbi, parcels, coordinates) {
   const features = new GeoJSON().readFeatures(parcels)
   const parcelSource = new VectorSource({ features })
-  const parcelLayer = new VectorLayer({ source: parcelSource, style: styleFunction })
-  const baseLayer = new TileLayer({ preload: Infinity, source: createBingMapsSource() })
-  const projection = createprojection()
+  const layers = buildMapLayers(parcelSource, apiKey)
+
+  const layerGroup = [
+    new Group({
+      title: 'Base maps',
+      layers
+    })
+  ]
 
   const view = new View({
     center: coordinates,
-    zoom: 14,
-    projection
+    zoom: 7,
+    extent: [-238375.0, 0.0, 900000.0, 1376256.0],
+    resolutions: tilegrid.getResolutions()
   })
 
   const map = new Map({ // eslint-disable-line no-unused-vars
-    layers: [
-      baseLayer,
-      parcelLayer
-    ],
+    layers: layerGroup,
     target: 'map',
     view
   })
+
+  const selectClick = new Select({
+    condition: click
+  })
+
+  const selectPointerMove = new Select({
+    condition: pointerMove,
+    style: pointerMoveStyle
+  })
+
+  map.addInteraction(selectClick)
+  map.addInteraction(selectPointerMove)
+
+  selectClick.on('select', function (e) {
+    window.location.href = `/parcel?sbi=${sbi}&sheetId=${e.selected[0].values_.sheet_id}&parcelId=${e.selected[0].values_.parcel_id}&mapStyle=${select.value}`
+  })
+
+  const select = document.getElementById('layer-select')
+
+  function onChange () {
+    const style = select.value
+    const totalLayers = layers.length - 1
+
+    for (let i = 0; i < totalLayers; ++i) {
+      layers[i].setVisible(mapStyles[i] === style)
+    }
+  }
+
+  select.addEventListener('change', onChange)
+
+  onChange()
+
+  hightlightOnMouseOver(parcelSource)
 }
