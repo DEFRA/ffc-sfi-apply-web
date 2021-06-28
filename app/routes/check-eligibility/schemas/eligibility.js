6ed5be8ca33f95bdf06a7e6171eef6a1d730c65a
David Stone <davidjamesstone@gmail.com>
2021-06-28 16:53:26 +0100

    Remove eligibility routes and views

--- app/routes/check-eligibility/schemas/eligibility.js
+++
@@ -1,7 +0,0 @@
-const joi = require('joi')
-
-module.exports = joi.object({
-  bps: joi.bool().required(),
-  landTypes: joi.alternatives().try(joi.array().required(), joi.string().required()),
-  farmingPilot: joi.bool().required()
-})
