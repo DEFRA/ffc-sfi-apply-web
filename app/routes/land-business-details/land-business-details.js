ef1eb4e42985494de7ef7b2042bb1dcd6f62c446
David Stone <davidjamesstone@gmail.com>
2021-06-28 16:55:49 +0100

    Relocate eligibilityCheckMessage call

--- app/routes/land-business-details/land-business-details.js
+++ app/routes/land-business-details/land-business-details.js
@@ -1,12 +1,29 @@
 const cache = require('../../cache')
+const { sendEligibilityCheckMessage } = require('../../messaging')
 
-module.exports = {
-  method: 'GET',
-  path: '/land-business-details',
-  options: {
-    handler: async (request, h) => {
-      const agreement = await cache.get('agreement', request.yar.id)
-      return h.view('land-business-details/land-business-details', { sbi: agreement.sbi })
+module.exports = [
+  {
+    method: 'GET',
+    path: '/land-business-details',
+    options: {
+      handler: async (request, h) => {
+        const agreement = await cache.get('agreement', request.yar.id)
+        return h.view('land-business-details/land-business-details', { sbi: agreement.sbi })
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/land-business-details',
+    options: {
+      handler: async (request, h) => {
+        const agreement = await cache.get('agreement', request.yar.id)
+        const { sbi } = agreement
+
+        await sendEligibilityCheckMessage({ sbi }, request.yar.id)
+
+        return h.redirect('/check-eligibility/eligible')
+      }
     }
   }
-}
+]
