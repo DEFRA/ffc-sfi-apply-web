2d2fe8c2204b092f164e8a3ce6b5e1bda4514955
David Stone <davidjamesstone@gmail.com>
2021-06-30 11:25:27 +0100

    Update eligibility message to meet contract (#46)
    * Update package version
    
    * Remove eligibility routes and views
    
    * Add start route
    
    * Remove eligibility link from home page
    
    * Relocate eligibilityCheckMessage call
    
    * Remove progress update
    
    * 1.16.3

---
+++ app/routes/start.js
@@ -0,0 +1,12 @@
+const cache = require('../cache')
+
+module.exports = {
+  method: 'GET',
+  path: '/start',
+  handler: async (request, h) => {
+    await cache.clear('progress', request.yar.id)
+    await cache.clear('agreement', request.yar.id)
+
+    return h.redirect('/application-task-list')
+  }
+}
