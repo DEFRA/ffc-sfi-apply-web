9bc6c63e03e0dde018c5ab8e6a553e579ebecd86
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-01 16:14:11 +0100

    console .log CH url

--- app/api/crown-hosting/base.js
+++ app/api/crown-hosting/base.js
@@ -2,6 +2,7 @@ const wreck = require('@hapi/wreck')
 const config = require('../../config')
 
 async function get (url, token) {
+  console.log(`CH API GATEWAY: ${config.chApi}${url}`)
   return wreck.get(`${config.chApi}${url}`, { rejectUnauthorized: false })
 }
 
