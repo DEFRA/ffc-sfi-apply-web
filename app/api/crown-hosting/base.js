2df4f8f2df5a05da82a69e1d80dda5f3611ab8dd
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-01 15:45:41 +0100

    Add ch url from config

--- app/api/crown-hosting/base.js
+++ app/api/crown-hosting/base.js
@@ -1,7 +1,8 @@
 const wreck = require('@hapi/wreck')
+const config = require('../../config')
 
 async function get (url, token) {
-  return wreck.get(`${url}`, { rejectUnauthorized: false })
+  return wreck.get(`${config.chApi}${url}`, { rejectUnauthorized: false })
 }
 
 module.exports = {
--- app/api/crown-hosting/organisation.js
+++ app/api/crown-hosting/organisation.js
@@ -1,7 +1,7 @@
 const { get } = require('./base')
 
 const getOrganisation = async (sbi) => {
-  const url = `https://ffc-sfi-api-gateway-pr1.ffc.snd.azure.defra.cloud/organisation/search/sbi/${sbi}`
+  const url = `organisation/search/sbi/${sbi}`
   const organisation = await get(url)
   return JSON.parse(organisation.payload.toString())._data[0]
 }
