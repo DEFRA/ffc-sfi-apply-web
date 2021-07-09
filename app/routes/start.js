efb019c4b990ed045ed44ffb9bb5f1b6f2f6fdd5
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-07-09 13:38:00 +0100

    changed start page to navigate to new and current user journeys

--- app/routes/start.js
+++ app/routes/start.js
@@ -1,12 +1,22 @@
 const cache = require('../cache')
 
-module.exports = {
+module.exports = [{
   method: 'GET',
   path: '/start',
   handler: async (request, h) => {
     await cache.clear('progress', request.yar.id)
     await cache.clear('agreement', request.yar.id)
 
+    return h.redirect('/application-task-list')
+  }
+},
+{
+  method: 'GET',
+  path: '/startV2',
+  handler: async (request, h) => {
+    await cache.clear('progress', request.yar.id)
+    await cache.clear('agreement', request.yar.id)
+
     return h.redirect('/v2/search-crn')
   }
-}
+}]
