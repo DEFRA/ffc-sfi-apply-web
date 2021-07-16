caae023b5409989b78568a639ca483cab63e6cb0
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-07-16 08:31:07 +0100

    changes for getting standards

--- app/routes/v2/standards/index.js
+++ app/routes/v2/standards/index.js
@@ -1,19 +1,22 @@
 const ViewModel = require('./models/select-standard')
+const { sendStandardsRequestMessage } = require('../../../messaging')
 const cache = require('../../../cache')
+const getPollingResponse = require('../../../polling')
 
 module.exports = [{
   method: 'GET',
   path: '/v2/standards',
   options: {
     handler: async (request, h) => {
-      await cache.update('apply-journey', request.yar.id, {
-        availableStandards: [
-          { text: 'Arable Soils', value: 'arableSoils' },
-          { text: 'Grassland Soils', value: 'grasslandSoils' }
-        ]
-      })
       const applyJourney = await cache.get('apply-journey', request.yar.id)
-      return h.view('v2/standards/select-standard', new ViewModel(applyJourney.availableStandards))
+      await sendStandardsRequestMessage(applyJourney, request.yar.id)
+      const response = await getPollingResponse(request.yar.id, '/standards')
+      if (response) {
+        console.info('Standards request received', response)
+        await cache.update('apply-journey', request.yar.id, { standards: response.standards })
+        return h.view('v2/standards/select-standard', new ViewModel(response.standards, applyJourney.selectedStandards))
+      }
+      return h.view('no-response')
     }
   }
 }]
