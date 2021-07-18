158458437ee1d4d446848b7c978b5c69773b75f8
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-07-18 20:35:19 +0100

    Get Eligbility

--- app/routes/proto/sfi-eligibility.js
+++ app/routes/proto/sfi-eligibility.js
@@ -1,8 +1,18 @@
+const crownHosting = require('./crown-hosting-interface')
+
 module.exports = [
   {
     method: 'GET',
     path: '/proto/sfi-eligibility',
     handler: async (request, h) => {
+      const callerId = request.yar.get('callerId')
+
+      const newSitiAPIEligible = await crownHosting.checkEligibilityNewSitiAPI(callerId, request.yar.get('chosen-sbi'))
+      const oldSitiAPIEligible = await crownHosting.checkEligibilityOldSitiAPI(callerId, request.yar.get('chosen-org-id'))
+
+      console.log(`newSitiAPIEligible: ${newSitiAPIEligible}`)
+      console.log(`oldSitiAPIEligible: ${oldSitiAPIEligible}`)
+
       return h.view('proto/sfi-eligibility')
     }
   },
