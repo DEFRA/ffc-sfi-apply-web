5d66ef1b382cdc378d53799dd1181f25a6a56639
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-07-18 12:43:43 +0100

    More CH calls

--- app/routes/proto/crown-hosting-interface/index.js
+++ app/routes/proto/crown-hosting-interface/index.js
@@ -15,12 +15,44 @@ async function getSBIs (callerId) {
   const endpoint = `organisation/person/${callerId}/summary?search=`
   const response = await callCrownHosting(endpoint, callerId)
 
-  return response?.payload?._data?.map(organisation => ({
+  const sbis = response?.payload?._data?.map(organisation => ({
     sbi: organisation.sbi,
     organisationId: organisation.id
   }))
+
+  return sbis ?? null
+}
+
+async function getOrgDetails (callerId, orgId) {
+  const endpoint = `organisation/${orgId}`
+  const response = await callCrownHosting(endpoint, callerId)
+
+  return response?.payload?._data
+    ? {
+      name: response.payload._data?.name,
+      address: response.payload._data?.address?.address1,
+      postcode: response.payload._data?.address?.postalCode
+    }
+    : null
+}
+
+async function checkEligibilityNewSitiAPI (callerId, sbi) {
+  const endpoint = `api/v1/sfi/eligibility/${sbi}`
+  const response = await callCrownHosting(endpoint, callerId)
+
+  return response?.payload?.elibigible === 'true'
+}
+
+async function checkEligibilityOldSitiAPI (callerId, orgId) {
+  const endpoint = `SitiAgriApi/authorisation/organisation/${orgId}/byFunction?functions=submitELMApplications&module=CUST_SS_PORTAL`
+  const response = await callCrownHosting(endpoint, callerId)
+
+  return response?.payload?.data?.submitELMApplications
 }
 
 module.exports = {
-  getSBIs
+  getSBIs,
+  getOrgDetails,
+  checkEligibilityNewSitiAPI,
+  checkEligibilityOldSitiAPI
 }
--- app/routes/proto/org-details.js
+++ app/routes/proto/org-details.js
@@ -1,8 +1,14 @@
+const crownHosting = require('./crown-hosting-interface')
+
 module.exports = [
   {
     method: 'GET',
     path: '/proto/org-details',
     handler: async (request, h) => {
+      const orgDetails = await crownHosting.getOrgDetails(
+        request.yar.get('callerId'),
+        request.yar.get('chosen-org-id')
+      )
       return h.view('proto/org-details')
     }
   },
