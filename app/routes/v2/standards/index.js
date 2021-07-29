51f8da18f6dba92d5edd93931d144ddbea266277
Fareed Mohammed <fareed.mohammed.uk@gmail.com>
2021-07-29 16:00:10 +0100

    Create Agreement Number (#61)
    * Create agreement number if not returned in standards response
    https://eaflood.atlassian.net/browse/SFI-675

--- app/routes/v2/standards/index.js
+++ app/routes/v2/standards/index.js
@@ -3,6 +3,7 @@ const joi = require('joi')
 const { sendStandardsRequestMessage } = require('../../../messaging')
 const cache = require('../../../cache')
 const getPollingResponse = require('../../../polling')
+const generateAgreementNumber = require('../../../agreement-number')
 
 module.exports = [{
   method: 'GET',
@@ -14,7 +15,7 @@ module.exports = [{
       const response = await getPollingResponse(request.yar.id, '/standards')
       if (response) {
         console.info('Standards request received', response)
-        await cache.update('apply-journey', request.yar.id, { agreementNumber: response.agreementNumber, standards: response.standards })
+        await cache.update('apply-journey', request.yar.id, { agreementNumber: response.agreementNumber ?? generateAgreementNumber(), standards: response.standards })
         return h.view('v2/standards/select-standard', new ViewModel(response.standards, applyJourney.selectedStandards))
       }
       return h.view('no-response')
