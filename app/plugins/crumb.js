871cacad0d2598878d3bbf417003bdc7c315e33b
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-08-05 15:06:55 +0100

    Testing JMeter (#63)
    * Remove bas64json encoding from crumb cookie options so that it works with JMeter tests

--- app/plugins/crumb.js
+++ app/plugins/crumb.js
@@ -1,4 +1,5 @@
-const { cookieOptions } = require('../config')
+const cookieOptions = { ...require('../config').cookieOptions }
+delete cookieOptions.encoding
 
 module.exports = {
   plugin: require('@hapi/crumb'),
