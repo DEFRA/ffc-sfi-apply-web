d89b7e09be3b8fd93e48019b62f06d315300834e
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-09-16 16:19:58 +0100

    changes to aggregate land covers

--- app/api/crown-hosting/land-cover.js
+++ app/api/crown-hosting/land-cover.js
@@ -2,50 +2,27 @@ const { get } = require('./base')
 
 const getLandCover = async (organisationId, callerId) => {
   const parcels = await get(`/lms/organisation/${organisationId}/land-covers`, callerId)
-  parcels.payload.forEach(p => p.info.forEach(i => (i.area /= 10000.0)))
-
-  const standards = [
-    {
-      code: '130',
-      name: 'Permanent grassland',
-      parcels: [],
-      totalHa: 0
-    },
-    {
-      code: '110',
-      name: 'Arable land',
-      parcels: [],
-      totalHa: 0
-    }
-  ]
+  const landCovers = []
 
   for (const parcel of parcels.payload) {
     const infos = parcel.info
-    const totalHa = 0
 
-    for (const standard of standards) {
+    for (const info of infos) {
       let area = 0
-
-      // Sum the parcel area eligible for this standard
-      for (const info of infos) {
-        if (info.area > 0 && info.code === standard.code) {
-          area += info.area
-        }
-      }
-
-      if (area > 0) {
-        // Add the parcel with the adjusted area to the standard
-        standard.parcels.push({
-          id: parcel.id,
-          area // : Number(area).toFixed(2)
+      if (landCovers.find(x => x.name === info.name) !== undefined) {
+        const landCover = landCovers.find(x => x.name === info.name)
+        landCover.area += info.area /= 10000.0
+      } else {
+        area += info.area /= 10000.0
+        landCovers.push({
+          name: info.name,
+          area
         })
       }
-
-      standard.totalHa += totalHa + area
     }
   }
 
-  return standards
+  return landCovers
 }
 
 module.exports = {
--- app/routes/land-business-details/confirm-details.js
+++ app/routes/land-business-details/confirm-details.js
@@ -12,14 +12,14 @@ module.exports = [
         const landCoverDetails = await getLandCover(applyJourney.selectedSbi.organisationId, applyJourney.callerId)
         const organisationAddress = await getOrganisationAddress(applyJourney.selectedSbi, applyJourney.callerId)
 
-        const totalHa = landCoverDetails?.reduce((acc, cur) => acc + cur.totalHa, 0)
+        const totalHa = landCoverDetails?.reduce((acc, cur) => acc + cur.area, 0)
         return h.view('land-business-details/confirm-details',
           {
             sbi: applyJourney.selectedSbi.sbi,
             name: organisationAddress.name,
             address: organisationAddress.address,
             totalHa: Number(totalHa).toFixed(2),
-            standards: landCoverDetails
+            landCovers: landCoverDetails
           })
       }
     }
