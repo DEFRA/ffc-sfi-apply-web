c5464ad39a5bcb367ee10ce0d73ec0dc770e556c
Fareed Mohammed <fareed.mohammed.uk@gmail.com>
2021-09-17 15:03:11 +0100

    Update business summary page to include real land data for SBI (#81)
    https://eaflood.atlassian.net/browse/SFI-1043

--- app/routes/land-business-details/confirm-details.js
+++ app/routes/land-business-details/confirm-details.js
@@ -1,5 +1,6 @@
 const cache = require('../../cache')
 const getOrganisationAddress = require('../../organisation-address')
+const { getLandCovers } = require('../../api/crown-hosting/land-cover')
 
 module.exports = [
   {
@@ -8,12 +9,17 @@ module.exports = [
     options: {
       handler: async (request, h) => {
         const applyJourney = await cache.get('apply-journey', request.yar.id)
+        const landCoverDetails = await getLandCovers(applyJourney.selectedSbi.organisationId, applyJourney.callerId)
         const organisationAddress = await getOrganisationAddress(applyJourney.selectedSbi, applyJourney.callerId)
+
+        const totalHa = landCoverDetails?.reduce((acc, cur) => acc + cur.area, 0)
         return h.view('land-business-details/confirm-details',
           {
             sbi: applyJourney.selectedSbi.sbi,
             name: organisationAddress.name,
-            address: organisationAddress.address
+            address: organisationAddress.address,
+            totalHa: Number(totalHa).toFixed(2),
+            landCovers: landCoverDetails
           })
       }
     }
