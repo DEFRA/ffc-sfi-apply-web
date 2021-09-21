77f968b7c85d691dd74fbf9fce0f46a30e1be9fb
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-09-21 13:47:58 +0100

    Added submission

--- app/routes/submit-application.js
+++ app/routes/submit-application.js
@@ -1,4 +1,5 @@
 const cache = require('../cache')
+const { saveAgreement, submitAgreement } = require('../api/agreement')
 
 module.exports = [{
   method: 'GET',
@@ -14,9 +15,12 @@ module.exports = [{
   path: '/declaration',
   options: {
     handler: async (request, h) => {
-      await cache.update('progress', request.yar.id, {
+      const applyJourney = await cache.get('apply-journey', request.yar.id)
+      await submitAgreement(applyJourney.agreementNumber, applyJourney.selectedSbi.sbi)
+      const progress = await cache.update('progress', request.yar.id, {
         progress: { submitted: true }
       })
+      await saveAgreement(applyJourney, progress)
       return h.redirect('/confirmation')
     }
   }
