d12383da81fa8c658ea21e7f7e3b363fd2a7373f
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-09-23 08:51:49 +0100

    Remove unused route and envar

--- app/routes/start.js
+++ app/routes/start.js
@@ -9,14 +9,4 @@ module.exports = [{
 
     return h.redirect('/application-task-list')
   }
-},
-{
-  method: 'GET',
-  path: '/startV2',
-  handler: async (request, h) => {
-    await cache.clear('progress', request.yar.id)
-    await cache.clear('apply-journey', request.yar.id)
-
-    return h.redirect('/v2/search-crn')
-  }
 }]
