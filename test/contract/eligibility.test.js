930dd1a76055ba31629790b2e384da7d76704ed7
Fay Toward <fay.toward@rpa.gov.uk>
2021-10-14 16:36:38 +0100

    eligibility pact addef

--- test/contract/eligibility.test.js
+++ test/contract/eligibility.test.js
@@ -1,25 +1,56 @@
-const { MessageProviderPact } = require('@pact-foundation/pact')
-const createMessage = require('../../app/messaging/create-message')
-
-describe('Pact Verification', () => {
-  test('validates the expectations of ffc-sfi-agreement-calculator', async () => {
-    const eligibility = {
-      callerId: '5089433',
-      crn: 'Joe Bloggs'
-    }
-
-    const provider = new MessageProviderPact({
-      messageProviders: {
-        'a request for new eligibility': () => createMessage(eligibility).body
-      },
-      consumer: 'ffc-sfi-agreement-calculator',
-      provider: 'ffc-sfi-apply-web',
-      consumerVersionTags: ['main', 'dev', 'test', 'preprod', 'prod'],
-      pactBrokerUrl: process.env.PACT_BROKER_URL,
-      pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
-      pactBrokerPassword: process.env.PACT_BROKER_PASSWORD
+const mockSendMessage = jest.fn()
+jest.mock('ffc-messaging', () => {
+  return {
+    MessageSender: jest.fn().mockImplementation(() => {
+      return {
+        sendMessage: mockSendMessage,
+        closeConnection: jest.fn()
+      }
     })
+  }
+})
+jest.mock('../../app/eligibility')
+const path = require('path')
+const { MessageConsumerPact } = require('@pact-foundation/pact')
+const Matchers = require('@pact-foundation/pact/src/dsl/matchers')
+const cache = require('../../app/cache')
 
-    return provider.verify()
+describe('receiving an eligibility check from SFI application', () => {
+  let consumer
+  let receiver
+
+  beforeAll(async () => {
+    await cache.start()
+    consumer = new MessageConsumerPact({
+      provider: 'ffc-sfi-agreement-calculator',
+      consumer: 'ffc-sfi-apply-web',
+      log: path.resolve(process.cwd(), 'test-output', 'pact.log'),
+      dir: path.resolve(process.cwd(), 'test-output')
+    })
+    receiver = { completeMessage: jest.fn(), abandonMessage: jest.fn() }
+  })
+
+  afterAll(async () => {
+    await cache.flushAll()
+    await cache.stop()
+  })
+
+  test('new eligibility check sends response message and sets message complete', async () => {
+    await consumer
+      .given('message is valid')
+      .expectsToReceive('a new eligibility check')
+      .withContent([{
+        sbi: Matchers.integer(123456789),
+        name: Matchers.like('Fred Potter'),
+        organisationId: Matchers.like('123456789'),
+        address: Matchers.like('address1, address2, address3, postalCode')
+      }])
+      .withMetadata({
+        'content-type': 'application/json',
+        'correlation-id': 'f492c3f4-03b3-455b-8de9-b9b05e025434',
+        'message-id': '0373821b-dd8c-4c3a-9fd8-f384504aa7bc'
+      })
+      .verify(async eligibilityMessage => eligibilityMessage.eligibility)
+    expect(receiver.completeMessage).toHaveBeenCalledTimes(1)
   })
 })
