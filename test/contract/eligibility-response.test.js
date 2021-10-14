67d1496da8bd5a478fa8686c629f09589f3c7008
Fay Toward <fay.toward@rpa.gov.uk>
2021-10-14 17:28:18 +0100

    eligibility pact addef

--- test/contract/eligibility-response.test.js
+++ test/contract/eligibility-response.test.js
@@ -1,24 +1,46 @@
-const { MessageProviderPact } = require('@pact-foundation/pact')
-const createMessage = require('../../app/messaging/create-message')
-
-describe('Pact Verification', () => {
-  test('eligibity satisifies all reponses', async () => {
-    const eligibility = {
-      callerId: '5089433',
-      crn: '1234567890'
-    }
-
-    const provider = new MessageProviderPact({
-      messageProviders: {
-        'a request for new eligibility check': () => createMessage(eligibility).body
-      },
-      provider: 'ffc-sfi-apply-web',
-      consumerVersionTags: ['main', 'dev', 'test', 'preprod', 'prod'],
-      pactBrokerUrl: process.env.PACT_BROKER_URL,
-      pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
-      pactBrokerPassword: process.env.PACT_BROKER_PASSWORD
+const mockSendMessage = jest.fn()
+jest.mock('ffc-messaging', () => {
+  return {
+    MessageSender: jest.fn().mockImplementation(() => {
+      return {
+        sendMessage: mockSendMessage,
+        closeConnection: jest.fn()
+      }
     })
+  }
+})
+jest.mock('../../app/eligibility')
+const path = require('path')
+const { MessageConsumerPact } = require('@pact-foundation/pact')
+const Matchers = require('@pact-foundation/pact/src/dsl/matchers')
 
-    return provider.verify()
+describe('receiving an eligibility check from SFI application', () => {
+  let consumer
+
+  beforeAll(async () => {
+    consumer = new MessageConsumerPact({
+      provider: 'ffc-sfi-agreement-calculator',
+      consumer: 'ffc-sfi-apply-web',
+      log: path.resolve(process.cwd(), 'test-output', 'pact.log'),
+      dir: path.resolve(process.cwd(), 'test-output')
+    })
+  })
+
+  test('new eligibility check sends response message and sets message complete', async () => {
+    await consumer
+      .given('message is valid')
+      .expectsToReceive('a new eligibility check')
+      .withContent([{
+        sbi: Matchers.integer(123456789),
+        name: Matchers.like('Fred Potter'),
+        organisationId: Matchers.like('123456789'),
+        address: Matchers.like('address1, address2, address3, postalCode')
+      }])
+      .withMetadata({
+        'content-type': 'application/json',
+        'correlation-id': 'f492c3f4-03b3-455b-8de9-b9b05e025434',
+        'message-id': '0373821b-dd8c-4c3a-9fd8-f384504aa7bc'
+      })
+      .verify(async eligibilityMessage => eligibilityMessage.eligibility)
   })
 })
--- test/contract/eligibility.test.js
+++ test/contract/eligibility.test.js
@@ -1,56 +1,24 @@
-const mockSendMessage = jest.fn()
-jest.mock('ffc-messaging', () => {
-  return {
-    MessageSender: jest.fn().mockImplementation(() => {
-      return {
-        sendMessage: mockSendMessage,
-        closeConnection: jest.fn()
-      }
+const { MessageProviderPact } = require('@pact-foundation/pact')
+const createMessage = require('../../app/messaging/create-message')
+
+describe('Pact Verification', () => {
+  test('eligibity satisifies all reponses', async () => {
+    const eligibility = {
+      callerId: '5089433',
+      crn: '1234567890'
+    }
+
+    const provider = new MessageProviderPact({
+      messageProviders: {
+        'a request for new eligibility check': () => createMessage(eligibility).body
+      },
+      provider: 'ffc-sfi-apply-web',
+      consumerVersionTags: ['main', 'dev', 'test', 'preprod', 'prod'],
+      pactBrokerUrl: process.env.PACT_BROKER_URL,
+      pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
+      pactBrokerPassword: process.env.PACT_BROKER_PASSWORD
     })
-  }
-})
-jest.mock('../../app/eligibility')
-const path = require('path')
-const { MessageConsumerPact } = require('@pact-foundation/pact')
-const Matchers = require('@pact-foundation/pact/src/dsl/matchers')
-const cache = require('../../app/cache')
 
-describe('receiving an eligibility check from SFI application', () => {
-  let consumer
-  let receiver
-
-  beforeAll(async () => {
-    await cache.start()
-    consumer = new MessageConsumerPact({
-      provider: 'ffc-sfi-agreement-calculator',
-      consumer: 'ffc-sfi-apply-web',
-      log: path.resolve(process.cwd(), 'test-output', 'pact.log'),
-      dir: path.resolve(process.cwd(), 'test-output')
-    })
-    receiver = { completeMessage: jest.fn(), abandonMessage: jest.fn() }
-  })
-
-  afterAll(async () => {
-    await cache.flushAll()
-    await cache.stop()
-  })
-
-  test('new eligibility check sends response message and sets message complete', async () => {
-    await consumer
-      .given('message is valid')
-      .expectsToReceive('a new eligibility check')
-      .withContent([{
-        sbi: Matchers.integer(123456789),
-        name: Matchers.like('Fred Potter'),
-        organisationId: Matchers.like('123456789'),
-        address: Matchers.like('address1, address2, address3, postalCode')
-      }])
-      .withMetadata({
-        'content-type': 'application/json',
-        'correlation-id': 'f492c3f4-03b3-455b-8de9-b9b05e025434',
-        'message-id': '0373821b-dd8c-4c3a-9fd8-f384504aa7bc'
-      })
-      .verify(async eligibilityMessage => eligibilityMessage.eligibility)
-    expect(receiver.completeMessage).toHaveBeenCalledTimes(1)
+    return provider.verify()
   })
 })
