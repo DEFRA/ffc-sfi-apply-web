30a736d68755a27cacc4af10d89e39a73234dbe9
Fay Toward <fay.toward@rpa.gov.uk>
2021-10-26 15:18:12 +0100

    spatial topic and subscriptions added

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -37,8 +37,17 @@ async function sendRequestSBIMessage (payload, correlationId) {
   console.info('SBI(s) requested')
 }
 
-async function receiveStandardsResponseMessage (messageId) {
+async function sendParcelSpatialMessage (payload, correlationId) {
+  await sendMessage(payload, 'uk.gov.sfi.sbi.parcelspatial', correlationId, config.parcelSpatialTopic)
+  console.info('parcel spatial data requested')
+}
+
+async function recieveParcelSpatialMessage (messageId) {
   return receiveMessage(messageId, config.responseStandardsQueue)
+}
+
+async function receiveStandardsResponseMessage (messageId) {
+  return receiveMessage(messageId, config.responseParcelSpatialQueue)
 }
 
 async function receiveEligibilityResponseMessage (messageId) {
@@ -57,6 +66,8 @@ module.exports = {
   sendAgreementSubmitMessage,
   sendAgreementWithdrawMessage,
   sendRequestSBIMessage,
+  sendParcelSpatialMessage,
+  recieveParcelSpatialMessage,
   receiveStandardsResponseMessage,
   receiveCalculateResponseMessage,
   receiveEligibilityResponseMessage
