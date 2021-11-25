ba29da25d1a257ff30efbd1b0fd200fd935aa727
John Watson <johnwatson484@hotmail.com>
2021-11-25 10:14:23 +0000

    Handle standards message

--- app/routes/funding-options/models/util-what-funding.js
+++ app/routes/funding-options/models/util-what-funding.js
@@ -1,6 +1,7 @@
 const cache = require('../../../cache')
 const { sendStandardsRequestMessage, receiveStandardsResponseMessage } = require('../../../messaging')
 const { v4: uuidv4 } = require('uuid')
+const util = require('util')
 
 const getAllStandards = async (request, error) => {
   const agreement = await cache.get('agreement', request.yar.id)
@@ -8,26 +9,23 @@ const getAllStandards = async (request, error) => {
   let standards = application.standards
   if (error && standards) {
     return { application, standards }
-  } else {
-    standards = await sendStandardsRequest(application, request, standards)
   }
-
+  standards = await sendStandardsRequest(application, request, standards)
   return { application, standards }
 }
 
 const sendStandardsRequest = async (application, request, standards) => {
   const messageId = uuidv4()
-  await sendStandardsRequestMessage(
-    {
-      sbi: application.selectedOrganisation.sbi,
-      organisationId: application.selectedOrganisation.organisationId,
-      callerId: application.callerId
-    }, request.yar.id, messageId)
+  await sendStandardsRequestMessage({
+    sbi: application.selectedOrganisation.sbi,
+    organisationId: application.selectedOrganisation.organisationId,
+    callerId: application.callerId
+  }, request.yar.id, messageId)
 
   const response = await receiveStandardsResponseMessage(messageId)
 
   if (response) {
-    console.info('Standards request received', response)
+    console.info('Standards request received', util.inspect(response, false, null, true))
     await cache.update('agreement', request.yar.id, { application: { standards: response.standards } })
     standards = response.standards
   }
