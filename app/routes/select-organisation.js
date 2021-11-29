ca68d0bfec36fdeee7e6418547d57cdee9c64448
FayToward <43171822+FayToward@users.noreply.github.com>
2021-11-29 16:48:21 +0000

    Update eligibility routing for single organisation (#116)

---
+++ app/routes/select-organisation.js
@@ -0,0 +1,28 @@
+const getEligibility = require('../eligibility')
+const cache = require('../cache')
+
+module.exports = [{
+  method: 'GET',
+  path: '/select-organisation',
+  options: {
+    handler: async (request, h) => {
+      const { eligibility } = await getEligibility(request)
+
+      if (!eligibility) {
+        return h.view('no-response')
+      }
+
+      if (!eligibility.length) {
+        return h.view('no-businesses')
+      }
+
+      if (eligibility.length === 1) {
+        const selectedOrganisation = eligibility[0]
+        await cache.update('agreement', request.yar.id, { application: { selectedOrganisation, submitted: false } })
+        return h.redirect('/start-application')
+      }
+
+      return h.redirect('/eligible-organisations')
+    }
+  }
+}]
--- test/integration/narrow/routes/eligible-organisations.test.js
+++ test/integration/narrow/routes/eligible-organisations.test.js
@@ -56,7 +56,7 @@ describe('eligible-organisations route', () => {
     await server.stop()
   })
 
-  test('GET /start-application Auth mode "required" token incorrect callerId', async () => {
+  test('GET /eligible-organisations Auth mode "required" token incorrect callerId', async () => {
     token = JWT.sign({ callerId: 9999 }, config.jwtConfig.secret)
     const options = {
       method: 'GET',
@@ -67,7 +67,7 @@ describe('eligible-organisations route', () => {
     expect(result.statusCode).toBe(302)
   })
 
-  test('GET /start-applicationAuth mode "required" token expired', async () => {
+  test('GET /eligible-organisations Auth mode "required" token expired', async () => {
     token = JWT.sign({ callerId }, config.jwtConfig.secret, { expiresIn: '1ms' })
     const options = {
       method: 'GET',
@@ -78,7 +78,7 @@ describe('eligible-organisations route', () => {
     expect(result.statusCode).toBe(302)
   })
 
-  test('GET /start-application Auth mode "required" should require header', async () => {
+  test('GET /eligible-organisations Auth mode "required" should require header', async () => {
     const options = {
       method: 'GET',
       url: '/eligible-organisations'
@@ -88,7 +88,7 @@ describe('eligible-organisations route', () => {
     expect(result.statusCode).toBe(302)
   })
 
-  test('GET /start-application Auth mode "required" should fail with invalid token', async () => {
+  test('GET /eligible-organisations Auth mode "required" should fail with invalid token', async () => {
     token = JWT.sign({ callerId }, 'bad secret')
     const options = {
       method: 'GET',
@@ -100,7 +100,7 @@ describe('eligible-organisations route', () => {
     expect(result.statusCode).toBe(302)
   })
 
-  test('GET /start-application returns 200', async () => {
+  test('GET /eligible-organisations returns 200', async () => {
     const options = {
       method: 'GET',
       url: '/eligible-organisations',
@@ -111,7 +111,7 @@ describe('eligible-organisations route', () => {
     expect(result.statusCode).toBe(200)
   })
 
-  test('GET /start-application returns start-application view', async () => {
+  test('GET /eligible-organisations returns eligible-organisations view', async () => {
     const options = {
       method: 'GET',
       url: '/eligible-organisations',
@@ -120,10 +120,10 @@ describe('eligible-organisations route', () => {
 
     const result = await server.inject(options)
     expect(result.request.response.variety).toBe('view')
-    expect(result.request.response.source.template).toBe('start-application')
+    expect(result.request.response.source.template).toBe('eligible-organisations')
   })
 
-  test('GET /start-application returns no-businesses view when no organisations returned', async () => {
+  test('GET /eligible-organisations returns no-businesses view when no organisations returned', async () => {
     const options = {
       method: 'GET',
       url: '/eligible-organisations',
