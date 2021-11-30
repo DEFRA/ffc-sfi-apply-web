87a83ca1f4c6f52d9f4103778182d24709245088
John Watson <johnwatson484@hotmail.com>
2021-11-30 09:10:21 +0000

    Update path

--- app/routes/confirmation.js
+++ app/routes/confirmation.js
@@ -6,7 +6,7 @@ module.exports = [{
   options: {
     handler: async (request, h) => {
       const agreement = await cache.get('agreement', request.yar.id)
-      return h.view('confirmation', { agreementNumber: agreement.agreementNumber })
+      return h.view('confirmation', { agreementNumber: agreement.application.agreementNumber })
     }
   }
 }]
