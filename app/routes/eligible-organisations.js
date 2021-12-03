eea7cd7f501a813a95effbfedc0d8c1f2efedd94
Paul Popa <paul.popa1995@yahoo.ro>
2021-12-03 11:52:52 +0000

    SFI-1248: Search for an organisation using SBI number - Updated package version - Added filter functionality - Added macro for search box - Added tests for new functionality

--- app/routes/eligible-organisations.js
+++ app/routes/eligible-organisations.js
@@ -1,4 +1,7 @@
+const cache = require('../cache')
 const getEligibility = require('../eligibility')
+const ViewModel = require('./models/search')
+const schema = require('./schemas/sbi')
 
 module.exports = [{
   method: 'GET',
@@ -19,7 +22,30 @@ module.exports = [{
         return h.redirect(`/start-application?sbi=${eligibility[0].sbi}`)
       }
 
-      return h.view('eligible-organisations', { organisations: eligibility })
+      return h.view('eligible-organisations', { organisations: eligibility, ...new ViewModel() })
+    }
+  }
+},
+{
+  method: 'POST',
+  path: '/eligible-organisations',
+  options: {
+    validate: {
+      payload: schema,
+      failAction: async (request, h, error) => {
+        return h.view('eligible-organisations', new ViewModel(request.payload.sbi, error)).code(400).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      const sbi = request.payload.sbi
+      const agreement = await cache.get('agreement', request.yar.id)
+      const organisations = agreement.application.eligibleOrganisations
+      const organisation = organisations.filter(organisation => organisation.sbi === sbi)
+
+      if (organisation.length) {
+        return h.view('eligible-organisations', { organisations: organisation, ...new ViewModel(sbi) })
+      }
+      return h.view('eligible-organisations', new ViewModel(sbi, { message: 'No organisation matching SBI.' })).code(400)
     }
   }
 }]
