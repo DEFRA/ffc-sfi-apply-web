0bb1c3bda7ff2d9d8fdfc229e7afa977b397412d
Paul Popa <paul.popa1995@yahoo.ro>
2021-12-03 11:52:52 +0000

    SFI-1248: Pseudocode for filter functionality

--- app/routes/eligible-organisations.js
+++ app/routes/eligible-organisations.js
@@ -22,4 +22,28 @@ module.exports = [{
       return h.view('eligible-organisations', { organisations: eligibility })
     }
   }
+},
+{
+  method: 'POST',
+  path: '/eligible-organisations',
+  options: {
+    handler: async (request, h) => {
+      const { eligibility } = await getEligibility(request)
+
+      // get the organisations from the cache
+      const organisations = cache.get('agreement', request.yar.id, { application: { eligibleOrganisations: eligibility } })
+      const sbi = request.payload.sbi
+
+      // match sbi in the orginsations array
+      const organisation = [] // cast to array
+
+      const json = {
+        organisations: organisation,
+        unavaible: true, // bool value,
+        sbi: request.payload.sbi
+      }
+
+      return h.view('eligible-organisations', json)
+    }
+  }
 }]
