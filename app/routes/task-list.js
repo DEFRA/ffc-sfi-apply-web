c8c781f30e32fe7f3128ee2a38c66844714f94b7
John Watson <johnwatson484@hotmail.com>
2021-12-10 19:47:56 +0000

    Update Docker

--- app/routes/task-list.js
+++ app/routes/task-list.js
@@ -1,10 +1,8 @@
-const Joi = require('joi')
 const ViewModel = require('./models/task-list')
 const cache = require('../cache')
 const paymentLevels = require('./payment-levels')
-const { getAgreementsBySbi, getAgreement, getProgress } = require('../api/agreement')
 
-module.exports = [{
+module.exports = {
   method: 'GET',
   path: '/task-list',
   options: {
@@ -15,35 +13,7 @@ module.exports = [{
       const fundingOption = application?.selectedStandard?.code === 'sfi-improved-grassland' ? 'improved-grassland-soils' : 'arable-soils'
       const paymentLevel = paymentLevels.find(x => x.name === application?.selectedAmbitionLevel?.name)
       const selectedOrganisation = application?.selectedOrganisation
-      const savedAgreements = await getAgreementsBySbi(selectedOrganisation.sbi)
-      return h.view('task-list', new ViewModel(progress, fundingOption, paymentLevel?.paymentLevel, savedAgreements.agreements, selectedOrganisation))
+      return h.view('task-list', new ViewModel(progress, fundingOption, paymentLevel?.paymentLevel, selectedOrganisation))
     }
   }
-},
-{
-  method: 'GET',
-  path: '/task-list/{agreementNumber}/{sbi}',
-  options: {
-    validate: {
-      params: Joi.object().keys({
-        agreementNumber: Joi.string().required(),
-        sbi: Joi.string().required()
-      }),
-      failAction: async (request, h, error) => {
-        return h.response('Bad request').code(400).takeover()
-      }
-    },
-    handler: async (request, h) => {
-      const agreement = await getAgreement(request.params.agreementNumber, request.params.sbi)
-      const progress = await getProgress(agreement.progressId)
-      agreement.agreementData.agreement.agreementNumber = request.params.agreementNumber
-      await cache.clear(request)
-      await cache.update(request,
-        {
-          application: agreement.agreementData.agreement,
-          progress
-        })
-      return h.redirect('/task-list')
-    }
-  }
-}]
+}
