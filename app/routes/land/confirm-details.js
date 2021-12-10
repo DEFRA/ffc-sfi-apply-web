5cccb5e06e544fc393ee9cb12026106e7dd5cb96
John Watson <johnwatson484@hotmail.com>
2021-12-10 21:39:05 +0000

    Update land

--- app/routes/land/confirm-details.js
+++ app/routes/land/confirm-details.js
@@ -2,48 +2,44 @@ const cache = require('../../cache')
 const getLand = require('../../land/land')
 const Joi = require('joi')
 
-module.exports = [
-  {
-    method: 'GET',
-    path: '/confirm-details',
-    options: {
-      handler: async (request, h) => {
+module.exports = [{
+  method: 'GET',
+  path: '/confirm-details',
+  options: {
+    handler: async (request, h) => {
+      const land = await getLand(request)
+
+      if (!land.parcels) {
+        return h.view('no-response')
+      }
+
+      return h.view('land/confirm-details', land)
+    }
+  }
+},
+{
+  method: 'POST',
+  path: '/confirm-details',
+  options: {
+    validate: {
+      payload: Joi.object({
+        isLandCorrect: Joi.boolean().required(),
+        'layer-select': Joi.string()
+      }),
+      failAction: async (request, h, error) => {
         const land = await getLand(request)
-
-        if (!land.parcels) {
-          return h.view('no-response')
-        }
-
-        return h.view('land/confirm-details', land)
+        return h.view('land/confirm-details', { ...land, errors: error }).code(400).takeover()
       }
-    }
-  },
-  {
-    method: 'POST',
-    path: '/confirm-details',
-    options: {
-      validate: {
-        payload: Joi.object({
-          isLandCorrect: Joi.boolean().required(),
-          'layer-select': Joi.string()
-        }),
-        failAction: async (request, h, error) => {
-          const mapParcels = await getLand(request)
-          mapParcels.errors = error
-          return h.view('land/confirm-details', mapParcels).code(400).takeover()
-        }
-      },
-      handler: async (request, h) => {
-        const payload = request.payload
+    },
+    handler: async (request, h) => {
+      const { isLandCorrect } = request.payload
+      await cache.update(request, { isLandCorrect })
 
-        if (payload.isLandCorrect) {
-          await cache.update(request, {
-            progress: { businessDetails: true }
-          })
-          return h.redirect('/management-control')
-        }
-
-        return h.redirect('/change-land-details')
+      if (isLandCorrect) {
+        return h.redirect('/management-control')
       }
+
+      return h.redirect('/change-land-details')
     }
-  }]
+  }
+}]
