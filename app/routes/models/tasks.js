726066090100c4b33f478d74253c513b48c829df
John Watson <johnwatson484@hotmail.com>
2021-12-10 22:00:30 +0000

    Set default

--- app/routes/models/tasks.js
+++ app/routes/models/tasks.js
@@ -1,7 +1,6 @@
 module.exports = [{
   id: 'land',
   taskGroup: 'Your land',
-  dependsOn: '',
   tasks: [{
     taskName: 'Confirm your land cover details',
     status: 'NOT STARTED',
@@ -11,44 +10,40 @@ module.exports = [{
 {
   id: 'funding',
   taskGroup: 'Choose your funding options',
-  dependsOn: 'land',
   tasks: [{
     id: 'fundingOption',
     dependsOn: 'funding',
     taskName: 'Choose funding option',
-    status: 'CANNOT START YET',
+    status: 'NOT STARTED',
     url: 'what-funding'
   }]
 },
 {
   id: 'action',
   taskGroup: 'Choose your actions',
-  dependsOn: 'fundingOption',
   tasks: [{
     id: 'actionOption',
     dependsOn: 'action',
     taskName: 'Choose your actions',
-    status: 'CANNOT START YET'
+    status: 'NOT STARTED'
   }]
 },
 {
   id: 'check',
   taskGroup: 'Check your answers',
-  dependsOn: 'actionOption',
   tasks: [{
     id: 'checkAnswers',
     dependsOn: 'check',
     taskName: 'Check your answers',
-    status: 'CANNOT START YET'
+    status: 'NOT STARTED'
   }]
 },
 {
   id: 'submit',
   taskGroup: 'Submit your application',
-  dependsOn: 'checkAnswers',
   tasks: [{
     taskName: 'Submit your application',
-    status: 'CANNOT START YET',
+    status: 'NOT STARTED',
     url: 'declaration'
   }]
 }]
