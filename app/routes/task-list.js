55794a8d30f51ef9c084fcfb5396883be403495a
John Watson <johnwatson484@hotmail.com>
2021-12-11 15:00:40 +0000

    Remove progress

--- app/routes/task-list.js
+++ app/routes/task-list.js
@@ -1,14 +1,11 @@
 const ViewModel = require('./models/task-list')
-const cache = require('../cache')
 
 module.exports = {
   method: 'GET',
   path: '/task-list',
   options: {
     handler: async (request, h) => {
-      const agreement = await cache.get(request)
-      const progress = agreement?.progress
-      return h.view('task-list', new ViewModel(progress))
+      return h.view('task-list', new ViewModel())
     }
   }
 }
