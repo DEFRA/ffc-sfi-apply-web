52eb05d1180d37d54bb42a7a4b00e9d1d765a214
John Watson <johnwatson484@hotmail.com>
2021-12-12 19:20:12 +0000

    Remove defunct

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,31 +1,8 @@
 const sendMessage = require('./send-message')
 const receiveMessage = require('./receive-message')
-const config = require('../config')
 
-async function sendAgreementValidateMessage (payload, correlationId) {
-  await sendMessage(payload, 'uk.gov.sfi.agreement.validate', correlationId, config.validateTopic)
-  console.info('Agreement validation requested')
-}
-
-async function sendAgreementCalculateMessage (payload, correlationId, messageId) {
-  await sendMessage(payload, 'uk.gov.sfi.agreement.calculate', correlationId, config.calculateTopic, messageId)
-  console.info('Agreement calculation requested')
-}
-
-async function sendAgreementSubmitMessage (payload, correlationId) {
-  await sendMessage(payload, 'uk.gov.sfi.agreement.submit', correlationId, config.submitTopic)
-  console.info('Agreement submitted')
-}
-
-async function receiveCalculateResponseMessage (messageId) {
-  return receiveMessage(messageId, config.responseCalculateQueue)
-}
 
 module.exports = {
-  sendAgreementValidateMessage,
-  sendAgreementCalculateMessage,
-  sendAgreementSubmitMessage,
-  receiveCalculateResponseMessage,
   sendMessage,
   receiveMessage
 }
