a7b44ccb739cdf01f3eafd93995f62a080e184a2
Simon Dunn <simondunn90@gmail.com>
2021-12-16 11:00:50 +0000

    SFI-1245: Paginate eligible organisations page (#126)
    Co-authored-by: Paul Popa <paul.popa1995@yahoo.ro>

--- test/integration/narrow/routes/eligible-organisations.test.js
+++ test/integration/narrow/routes/eligible-organisations.test.js
@@ -5,8 +5,9 @@ describe('eligible-organisations route', () => {
   jest.mock('ffc-messaging')
   jest.mock('../../../../app/plugins/crumb')
   jest.mock('../../../../app/eligibility')
-  const getEligibility = require('../../../../app/eligibility')
   jest.mock('../../../../app/cache')
+
+  const getEligibility = require('../../../../app/eligibility')
   const mockCache = require('../../../../app/cache')
 
   let createServer
@@ -26,6 +27,19 @@ describe('eligible-organisations route', () => {
     organisationId: 7654321,
     address: 'address1, address2, address3, postalCode'
   }]
+
+  const organisationsLarge = []
+
+  for (let index = 1; index < 77; index++) {
+    const organisation = {
+      sbi: 987654321 - index,
+      name: `Test name ${index}`,
+      organisationId: 7654321 - index,
+      address: `Test address ${index}`
+    }
+
+    organisationsLarge.push(organisation)
+  }
 
   beforeEach(async () => {
     token = JWT.sign({ callerId }, config.jwtConfig.secret)
@@ -114,9 +128,70 @@ describe('eligible-organisations route', () => {
   test('GET /eligible-organisations returns eligible-organisations view', async () => {
     const options = {
       method: 'GET',
+      url: '/eligible-organisations?page=4',
+      headers: { authorization: token }
+    }
+
+    const result = await server.inject(options)
+    expect(result.statusCode).toBe(200)
+  })
+
+  test('GET /eligible-organisations with no page query returns page defaults to 1', async () => {
+    const options = {
+      method: 'GET',
       url: '/eligible-organisations',
       headers: { authorization: token }
     }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.query.page).toBe(1)
+  })
+
+  test('GET /eligible-organisations with page query returns 200', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=4',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.variety).toBe('view')
+    expect(result.request.response.source.template).toBe('eligible-organisations')
+  })
+
+  test('GET /eligible-organisations with page query returns eligible-organisations view', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=4',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
 
     const result = await server.inject(options)
     expect(result.request.response.variety).toBe('view')
@@ -163,6 +238,279 @@ describe('eligible-organisations route', () => {
     const result = await server.inject(options)
     expect(result.request.response.variety).toBe('view')
     expect(result.request.response.source.template).toBe('no-response')
+  })
+
+  test('GET /eligible-organisations with over 10 organisations returns 200', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.statusCode).toBe(200)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations returns pagination flag as true', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.source.context.pagination).toBe(true)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations returns eligible-organisations view', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.variety).toBe('view')
+    expect(result.request.response.source.template).toBe('eligible-organisations')
+  })
+
+  test('GET /eligible-organisations with over 10 organisations and no page query returns page defaults to 1', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.query.page).toBe(1)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations and page query set to valid value returns eligible-organisations view on that page', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=2',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.variety).toBe('view')
+    expect(result.request.response.source.template).toBe('eligible-organisations')
+    expect(result.request.query.page).toBe(2)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations and page query set to exceeding page value returns eligible-organisations view on the last page', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=222',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.statusCode).toBe(200)
+    expect(result.request.response.variety).toBe('view')
+    expect(result.request.response.source.template).toBe('eligible-organisations')
+    expect(result.request.response.source.context.page).toBe(8)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations returns correct pagination meta data', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.source.context.totalPages).toBe(8)
+    expect(result.request.response.source.context.total).toBe(76)
+    expect(result.request.response.source.context.limit).toBe(10)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations on first page returns correct pagination flags', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.source.context.page).toBe(1)
+    expect(result.request.response.source.context.totalPages).toBe(8)
+    expect(result.request.response.source.context.hasNext).toBe(true)
+    expect(result.request.response.source.context.hasPrevious).toBe(false)
+    expect(result.request.response.source.context.showNextEllipsis).toBe(true)
+    expect(result.request.response.source.context.showPreviousEllipsis).toBe(false)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations on last page returns correct pagination flags', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=8',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.source.context.page).toBe(8)
+    expect(result.request.response.source.context.totalPages).toBe(8)
+    expect(result.request.response.source.context.hasNext).toBe(false)
+    expect(result.request.response.source.context.hasPrevious).toBe(true)
+    expect(result.request.response.source.context.showNextEllipsis).toBe(false)
+    expect(result.request.response.source.context.showPreviousEllipsis).toBe(true)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations on 2nd first page returns correct pagination flags', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=2',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.source.context.page).toBe(2)
+    expect(result.request.response.source.context.totalPages).toBe(8)
+    expect(result.request.response.source.context.hasNext).toBe(true)
+    expect(result.request.response.source.context.hasPrevious).toBe(true)
+    expect(result.request.response.source.context.showNextEllipsis).toBe(true)
+    expect(result.request.response.source.context.showPreviousEllipsis).toBe(false)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations on 2nd last page returns correct pagination flags', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=7',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.source.context.page).toBe(7)
+    expect(result.request.response.source.context.totalPages).toBe(8)
+    expect(result.request.response.source.context.hasNext).toBe(true)
+    expect(result.request.response.source.context.hasPrevious).toBe(true)
+    expect(result.request.response.source.context.showNextEllipsis).toBe(false)
+    expect(result.request.response.source.context.showPreviousEllipsis).toBe(true)
+  })
+
+  test('GET /eligible-organisations with over 10 organisations on middle page returns correct pagination flags', async () => {
+    const options = {
+      method: 'GET',
+      url: '/eligible-organisations?page=4',
+      headers: { authorization: token }
+    }
+
+    getEligibility.mockResolvedValue(
+      {
+        eligibility: organisationsLarge,
+        agreement: {
+          selectedOrganisation: []
+        }
+      }
+    )
+
+    const result = await server.inject(options)
+    expect(result.request.response.source.context.page).toBe(4)
+    expect(result.request.response.source.context.totalPages).toBe(8)
+    expect(result.request.response.source.context.hasNext).toBe(true)
+    expect(result.request.response.source.context.hasPrevious).toBe(true)
+    expect(result.request.response.source.context.showNextEllipsis).toBe(true)
+    expect(result.request.response.source.context.showPreviousEllipsis).toBe(true)
   })
 
   test('POST /eligible-organisations Auth mode "required" token incorrect callerId', async () => {
