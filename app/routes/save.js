4cc6923bfbf6ba5baa1aacc5e7b9bca62a530075
John Watson <johnwatson484@hotmail.com>
2021-12-17 12:39:04 +0000

    fixed save and return

--- app/routes/save.js
+++ app/routes/save.js
@@ -6,6 +6,14 @@ module.exports = [{
   options: {
     handler: async (request, h) => {
       await save(request)
+      return h.redirect('saved')
+    }
+  }
+}, {
+  method: 'GET',
+  path: '/saved',
+  options: {
+    handler: async (request, h) => {
       return h.view('saved')
     }
   }
