58d5d2a04bc94d4755968a0f923e85d9aed4b227
marctemp <marcte@kainos.com>
2022-01-05 13:03:20 +0000

    first pass at logic

--- app/routes/funding/models/what-funding.js
+++ app/routes/funding/models/what-funding.js
@@ -1,3 +1,24 @@
+const standardItems = {
+  'sfi-moorland': {
+    text: 'Funding for moorland or rough grazing',
+    value: 'sfi-moorland',
+    hint: { text: '£6.45 per hectare per year, plus a single payment of £148 per year.' },
+    checked: false
+  },
+  'sfi-arable-soil': {
+    text: 'Funding for arable and horticultural soil',
+    value: 'sfi-arable-soil',
+    hint: { text: '£22 to £40 per hectare per year based on what work you do.' },
+    checked: false
+  },
+  'sfi-improved-grassland': {
+    text: 'Funding for improved grassland soil',
+    value: 'sfi-improved-grassland',
+    hint: { text: '£28 to £58 per hectare per year based on what work you do.' },
+    checked: false
+  }
+}
+
 function ViewModel (eligibleFunding, selected, error) {
   this.model = {
     id: 'standard',
@@ -12,24 +33,7 @@ function ViewModel (eligibleFunding, selected, error) {
     hint: {
       text: 'Choose all that apply.'
     },
-    items: [{
-      text: 'Funding for moorland or rough grazing',
-      value: 'sfi-moorland',
-      hint: { text: '£6.45 per hectare per year, plus a single payment of £148 per year.' },
-      checked: false
-    },
-    {
-      text: 'Funding for arable and horticultural soil',
-      value: 'sfi-arable-soil',
-      hint: { text: '£22 to £40 per hectare per year based on what work you do.' },
-      checked: false
-    },
-    {
-      text: 'Funding for improved grassland soil',
-      value: 'sfi-improved-grassland',
-      hint: { text: '£28 to £58 per hectare per year based on what work you do.' },
-      checked: false
-    }]
+    items: mapStandards(eligibleFunding, selected)
   }
 
   if (error) {
@@ -43,22 +47,23 @@ function ViewModel (eligibleFunding, selected, error) {
   }
 }
 
-const isChecked = (selected, value) => {
-  if (selected.length) {
-    return selected.includes(value)
-  }
-  return false
+const mapStandards = (eligibleFunding, selected) => {
+  let standardObj = { ...standardItems }
+  standardObj = selected.map((choosen) => getChecked(choosen, standardObj))[0]
+  return getEligible(eligibleFunding, standardObj)
 }
 
-const mapStandards = (eligibleFunding, selected) => {
-  return eligibleFunding.filter(item => item.landCovers.length > 0)
-    .map(x => {
-      return {
-        text: x.name,
-        value: x.code,
-        checked: isChecked(selected, x.code)
-      }
-    })
+const getEligible = (eligibleFunding, standardObj) => {
+  const eligibleCodes = eligibleFunding.filter(item => item.landCovers.length > 0)
+    .map(standards => standards.code)
+  return eligibleCodes.map(code => standardObj[code])
+}
+
+const getChecked = (selected, standardObj) => {
+  if (Object.keys(standardObj).includes(selected)) {
+    standardObj[selected].checked = true
+  }
+  return standardObj
 }
 
 module.exports = ViewModel
