f48da6da7ada3c1845edae6104e4e0ca38bba5dd
marctemp <marcte@kainos.com>
2022-01-05 09:27:02 +0000

    mock cache and mock integration addded, appearance as expected

--- app/routes/funding/models/what-funding.js
+++ app/routes/funding/models/what-funding.js
@@ -9,12 +9,36 @@ function ViewModel (eligibleFunding, selected, error) {
         classes: 'govuk-fieldset__legend--l'
       }
     },
-    items: mapStandards(eligibleFunding, selected)
+    hint: {
+      text: 'Choose all that apply.'
+    },
+    items: [{
+      text: 'Funding for moorland or rough grazing',
+      value: 'sfi-moorland',
+      hint: { text: '£6.45 per hectare per year, plus a single payment of £148 per year.' },
+      checked: false
+    },
+    {
+      text: 'Funding for arable and horticultural soil',
+      value: 'sfi-arable-soil',
+      hint: { text: '£22 to £40 per hectare per year based on what work you do.' },
+      checked: false
+    },
+    {
+      text: 'Funding for improved grassland soil',
+      value: 'sfi-improved-grassland',
+      hint: { text: '£28 to £58 per hectare per year based on what work you do.' },
+      checked: false
+    }]
   }
 
   if (error) {
-    this.model.errorMessage = {
-      text: 'Please choose the funding you would like to apply for '
+    this.model.error = {
+      errorList: [
+        {
+          text: 'Select an option'
+        }
+      ]
     }
   }
 }
