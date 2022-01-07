1161a9924f8d9033dcac38efe3458715ab421a94
marctemp <marcte@kainos.com>
2022-01-07 15:33:22 +0000

    verifying commit

--- app/routes/funding/what-funding.js
+++ app/routes/funding/what-funding.js
@@ -1,4 +1,3 @@
-const { required } = require('joi')
 const Joi = require('joi')
 const cache = require('../../cache')
 const getFunding = require('../../funding/get-funding')
@@ -9,9 +8,6 @@ module.exports = [{
   path: '/what-funding',
   options: {
     handler: async (request, h) => {
-      const cacheMock = require('../../cache_mock')
-      cache.set(request, cacheMock[0]) // mimics the cache being built up before this page
-
       const { agreement } = await cache.get(request)
       const { funding } = agreement
 
@@ -34,8 +30,6 @@ module.exports = [{
         standard: Joi.required()
       }),
       failAction: async (request, h, error) => {
-        console.log(request.payload.standard)
-        return h.view('funding/what-funding', new ViewModel(1, 2, error)).code(400).takeover()
         const { agreement } = await cache.get(request)
         const { funding } = agreement
         const eligibleFunding = await getFunding(request) ?? undefined
@@ -46,7 +40,6 @@ module.exports = [{
       }
     },
     handler: async (request, h) => {
-<<<<<<< HEAD
       const standards = typeof (request.payload.standard) === 'string' ? [request.payload.standard] : request.payload.standard
       const { data, agreement } = await cache.get(request)
 
@@ -64,40 +57,6 @@ module.exports = [{
       cache.update(request, { agreement })
 
       return h.redirect('/how-much')
-=======
-      // const { standard } = request.payload
-      // const { data, agreement } = await cache.get(request)
-
-      // const funding = data?.eligibleFunding.filter(x => standard.includes(x.code)) ?? []
-
-      // if (!funding.length) {
-      //   return h.redirect('/what-funding')
-      // }
-
-      // for (const option in agreement.action) {
-      //   agreement.action[option].active = funding.some(x => x.code === option)
-      // }
-      // agreement.funding = funding.map(x => x.code)
-
-      // await cache.update(request, { agreement })
-      // return h.redirect('/how-much')
-
-      const { agreement } = await cache.get(request)
-      const { standard } = request.payload
-
-      if (typeof (standard) === 'string') {
-        agreement.action[standard] = { actionsComplete: true }
-      } else {
-        standard.map(option => {
-          agreement.action[option] = { actionsComplete: true }
-          return 1
-        })
-      }
-
-      cache.update(request, { agreement })
-
-      return h.redirect('/task-list')
->>>>>>> main
     }
   }
 }]
