316f1843dbfe9f27b795e2ff5615a3f762fc0513
John Watson <johnwatson484@hotmail.com>
2022-01-10 13:55:31 +0000

    Refactor sections

--- app/task-list/get-sections.js
+++ app/task-list/get-sections.js
@@ -18,43 +18,26 @@ const updateSections = (agreement) => {
     return sections
   }
 
-  const landSection = getSection(sections, 'Your land')
-  completeSection(landSection)
-
-  // set funding section ready to start
-  const fundingSection = getSection(sections, 'Choose your funding')
-  fundingSection.tasks[0].status = NOT_STARTED_YET
+  const fundingSection = PrepareFundingSection(sections)
 
   // if funding not started
   if (!funding.length) {
     return sections
   }
 
-  completeSection(fundingSection)
-
-  const actionSections = {
-    actionSection: getSection(sections, 'Choose your actions'),
-    arableSoilSection: getSection(sections, 'Arable and horticultural soil actions'),
-    improvedGrasslandSection: getSection(sections, 'Improved grassland soil actions'),
-    moorlandSection: getSection(sections, 'Moorlands and rough grazing actions')
-  }
-
-  updateActions(sections, actionSections, funding, action)
+  const actionSections = prepareActionSections(fundingSection, sections, funding, action)
 
   if (!actionsComplete(sections, actionSections, funding)) {
     return sections
   }
 
-  const checkSection = getSection(sections, 'Check your answers')
-  checkSection.tasks[0].status = NOT_STARTED_YET
+  const checkSection = prepareCheckSection(sections)
 
   if (!confirmed) {
     return sections
   }
 
-  completeSection(checkSection)
-  const submitSection = getSection(sections, 'Submit your application')
-  submitSection.tasks[0].status = NOT_STARTED_YET
+  const submitSection = prepareSubmitSection(checkSection, sections)
 
   // if confirmed but not submitted then update status
   if (!submitted) {
@@ -67,6 +50,45 @@ const updateSections = (agreement) => {
 
 const getSection = (sections, name) => {
   return sections.find(x => x.name === name)
+}
+
+const PrepareFundingSection = (sections) => {
+  const landSection = getSection(sections, 'Your land')
+  completeSection(landSection)
+
+  // set funding section ready to start
+  const fundingSection = getSection(sections, 'Choose your funding')
+  fundingSection.tasks[0].status = NOT_STARTED_YET
+  return fundingSection
+}
+
+const getActionSections = (sections) => {
+  return {
+    actionSection: getSection(sections, 'Choose your actions'),
+    arableSoilSection: getSection(sections, 'Arable and horticultural soil actions'),
+    improvedGrasslandSection: getSection(sections, 'Improved grassland soil actions'),
+    moorlandSection: getSection(sections, 'Moorlands and rough grazing actions')
+  }
+}
+
+const prepareActionSections = (fundingSection, sections, funding, action) => {
+  completeSection(fundingSection)
+  const actionSections = getActionSections(sections)
+  updateActions(sections, actionSections, funding, action)
+  return actionSections
+}
+
+const prepareCheckSection = (sections) => {
+  const checkSection = getSection(sections, 'Check your answers')
+  checkSection.tasks[0].status = NOT_STARTED_YET
+  return checkSection
+}
+
+const prepareSubmitSection = (checkSection, sections) => {
+  completeSection(checkSection)
+  const submitSection = getSection(sections, 'Submit your application')
+  submitSection.tasks[0].status = NOT_STARTED_YET
+  return submitSection
 }
 
 module.exports = getSections
