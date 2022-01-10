6aafe30003adf854f0905ac892b206531a176d63
Marc Templeton <marcte@kainos.com>
2022-01-10 10:46:47 +0000

    whitespace added to match previous code

--- app/routes/funding/what-funding.js
+++ app/routes/funding/what-funding.js
@@ -31,18 +31,22 @@ module.exports = [{
       failAction: async (request, h, error) => {
         const { agreement } = await cache.get(request)
         const { funding } = agreement
+
         const eligibleFunding = await getFunding(request) ?? undefined
+
         if (!eligibleFunding) {
           return h.view('no-response').takeover()
         }
+
         return h.view('funding/what-funding', new ViewModel(eligibleFunding, funding, error)).code(400).takeover()
       }
     },
     handler: async (request, h) => {
-      const standards = request.payload.standard
+      const { standard } = request.payload.standard
       const { data, agreement } = await cache.get(request)
 
-      const eligibleFunding = (data?.eligibleFunding ?? []).filter(fundingObj => standards.includes(fundingObj.code))
+      const eligibleFunding = (data?.eligibleFunding ?? []).filter(fundingObj => standard.includes(fundingObj.code))
+
       if (!eligibleFunding.length) {
         return h.redirect('/what-funding')
       }
