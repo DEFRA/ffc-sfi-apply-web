cb8e78226139350ef9e64896cb40e7ca76860251
Marc Templeton <marcte@kainos.com>
2022-01-12 13:43:45 +0000

    addressing standard linting issue

--- test/unit/what-funding.test.js
+++ test/unit/what-funding.test.js
@@ -28,8 +28,6 @@ describe('process standards message', () => {
   let createServer
   let server
 
-  let standardDetails
-
   const mockEligibleFunding = [{
     code: 'sfi-arable-soil',
     name: 'arable and horticultural soil',
@@ -50,29 +48,6 @@ describe('process standards message', () => {
     createServer = require('../../app/server')
     server = await createServer()
     await server.initialize()
-  })
-
-  beforeEach(async () => {
-    standardDetails = {
-      'sfi-moorland': {
-        text: 'Funding for moorland or rough grazing',
-        value: 'sfi-moorland',
-        hint: { text: '£6.45 per hectare per year, plus a single payment of £148 per year.' },
-        checked: false
-      },
-      'sfi-arable-soil': {
-        text: 'Funding for arable and horticultural soil',
-        value: 'sfi-arable-soil',
-        hint: { text: '£22 to £40 per hectare per year based on what work you do.' },
-        checked: false
-      },
-      'sfi-improved-grassland': {
-        text: 'Funding for improved grassland soil',
-        value: 'sfi-improved-grassland',
-        hint: { text: '£28 to £58 per hectare per year based on what work you do.' },
-        checked: false
-      }
-    }
   })
 
   afterEach(async () => {
