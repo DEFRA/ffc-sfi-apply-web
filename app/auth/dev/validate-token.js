96ad229ca4b067382e5e02b97fb8333a0e737223
John Watson <john.watson1@defra.gov.uk>
2023-09-21 14:44:27 +0100

    Restore sign out link

--- app/auth/dev/validate-token.js
+++ app/auth/dev/validate-token.js
@@ -2,7 +2,7 @@ const { createSession } = require('../create-session')
 
 const validateToken = async (decoded, request, _h) => {
   await createSession(request, decoded.sessionId, decoded.contactId)
-  return { isValid: true, credentials: { scope: decoded.roles, name: 'Andrew Shaughnessy', sessionId: decoded.sessionId, crn: decoded.contactId } }
+  return { isValid: true, credentials: { scope: decoded.roles, name: 'A Farmer', sessionId: decoded.sessionId, crn: decoded.contactId } }
 }
 
 module.exports = {
