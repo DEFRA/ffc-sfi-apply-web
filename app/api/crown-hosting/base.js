8242d9a8bda2d2967c834f0678ba2a7222607482
John Watson <42574977+johnwatson484@users.noreply.github.com>
2023-11-24 15:45:20 +0000

    Use APIM (#157)

--- app/api/crown-hosting/base.js
+++ app/api/crown-hosting/base.js
@@ -1,8 +1,40 @@
 const wreck = require('@hapi/wreck')
 const config = require('../../config')
+const FormData = require('form-data')
 
-async function get (url, crn, token) {
-  return wreck.get(`${config.chApiGateway}${url}`, { headers: { crn, Authorization: `Bearer ${token}` }, json: true, rejectUnauthorized: false })
+const get = async (url, crn, token) => {
+  const apimToken = await getApimToken()
+  const { payload } = await wreck.get(`${config.chApiGateway}${url}`, getConfiguration(crn, token, apimToken))
+  return payload
+}
+
+const getApimToken = async () => {
+  const data = new FormData()
+  data.append('client_id', config.apiConfig.apimClientId)
+  data.append('client_secret', config.apiConfig.apimClientSecret)
+  data.append('scope', config.apiConfig.apimScope)
+  data.append('grant_type', 'client_credentials')
+
+  const response = await wreck.post(config.apiConfig.apimAuthorizationUrl, {
+    headers: data.getHeaders(),
+    payload: data,
+    json: true
+  })
+
+  return `${response.payload.token_type} ${response.payload.access_token}`
+}
+
+const getConfiguration = (crn, token, apimToken) => {
+  return {
+    headers: {
+      crn,
+      'X-Forwarded-Authorization': token,
+      'Ocp-Apim-Subscription-Key': config.apiConfig.apimOcpSubscriptionKey,
+      Authorization: apimToken
+    },
+    json: true,
+    rejectUnauthorized: false
+  }
 }
 
 module.exports = {
